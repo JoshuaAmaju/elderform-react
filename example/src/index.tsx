2181bcbdfbce2c97e8b85298cde38b7da7f7f3f8
Joshua Amaju <princejosh0007@gmail.com>
2022-11-05 01:33:13 +0100

    chore: add vite

--- example/src/index.tsx
+++ example/src/index.tsx
@@ -1,13 +1,27 @@
-import React, { useEffect } from 'react';
-import { create } from 'elderform';
+import { create } from '../../../core/src';
+import { nanoid } from 'nanoid';
+import React from 'react';
 import * as z from 'zod';
+import * as y from 'yup';
+import { get } from 'object-path';
 import { Field, Form, useForm } from '../../src';
 
 const string = (v: string) => z.string().parseAsync(v);
 
-const email = (v: string) => z.string().email().parseAsync(v);
+const email = (v: string) => y.string().email().required().validate(v);
 
 const form = create({
+  initialValues: {
+    friends: [],
+    // friends: {
+    //   // [nanoid()]: {
+    //   //   name: '',
+    //   // },
+    // },
+  },
+  // initialErrors: {
+  //   friends: {}
+  // },
   onSubmit() {
     return new Promise((resolve, reject) => {
       setTimeout(resolve, 3000);
@@ -15,16 +29,144 @@ const form = create({
   },
 });
 
+const _App = () => {
+  const state = useForm(form);
+
+  const { friends } = state.values;
+
+  // console.log(state.actors, state.values, state.errors);
+
+  return (
+    <Form state={state}>
+      <div>
+        {friends.map((id, i) => {
+          // const value = friends[id];
+          const name = `friends.${i}.name`;
+          // const error = state.errors.friends?.[id];
+
+          console.log(state.errors.friends?.[i]);
+
+          return (
+            <div key={i}>
+              <label htmlFor={name}>Name</label>
+
+              <Field name={name} onValidate={email}>
+                {(f) => {
+                  return (
+                    <>
+                      <input
+                        value={f.value}
+                        onChange={(e) => f.set(e.target.value)}
+                      />
+
+                      <button
+                        onClick={() => {
+                          // state.kill(name);
+                          // delete friends[id];
+                          // console.log(i, friends.splice(i, 1));
+                          // friends.splice(i, 1);
+
+                          friends.splice(i, 1);
+                          state.set('friends', friends);
+                        }}
+                      >
+                        remove
+                      </button>
+
+                      <button onClick={() => f.validate()}>validate</button>
+
+                      {f.error && <p>{JSON.stringify(f.error)}</p>}
+                    </>
+                  );
+                }}
+              </Field>
+            </div>
+          );
+        })}
+
+        <button
+          type="button"
+          onClick={() => {
+            // state.spawn(`friends.${nanoid()}.name`, '', email);
+
+            state.set('friends', [...friends, { name: '' }] as any);
+          }}
+        >
+          Add
+        </button>
+      </div>
+    </Form>
+  );
+
+  // return (
+  //   <div>
+  //     {Object.keys(friends).map((id) => {
+  //       const value = friends[id];
+  //       const name = `friends.${id}.name`;
+  //       const error = state.errors.friends?.[id];
+
+  //       return (
+  //         <div key={id}>
+  //           <label htmlFor={name}>Name</label>
+
+  //           <input
+  //             onChange={(e) => {
+  //               state.set('friends', {
+  //                 ...friends,
+  //                 [id]: { name: e.target.value },
+  //               });
+  //             }}
+  //           />
+
+  //           <button
+  //             onClick={() => {
+  //               // state.kill(name);
+  //               delete friends[id];
+  //               state.set('friends', { ...friends });
+  //             }}
+  //           >
+  //             remove
+  //           </button>
+
+  //           <button onClick={() => state.validate(name)}>validate</button>
+
+  //           {error?.name && <p>{JSON.stringify(error.name)}</p>}
+  //         </div>
+  //       );
+  //     })}
+
+  //     <button
+  //       type="button"
+  //       onClick={() => {
+  //         // state.spawn(`friends.${nanoid()}.name`, '', email);
+
+  //         state.set('friends', { ...friends, [nanoid()]: '' });
+  //       }}
+  //     >
+  //       Add
+  //     </button>
+  //   </div>
+  // );
+};
+
 const App = () => {
   const state = useForm(form);
 
-  console.log(state.values);
+  const { friends } = state.values;
 
-  useEffect(() => {
-    if (state.submitted) {
-      alert(JSON.stringify(state.values));
-    }
-  }, [state.values, state.submitted]);
+  // console.log(friends);
+
+  console.log(state.values, state.errors);
+
+  // console.log(state.values);
+
+  // useEffect(() => {
+  //   if (state.submitted) {
+  //     alert(JSON.stringify(state.values));
+  //   }
+  // }, [state.values, state.submitted]);
+
+  // console.log(get(state.actors, 'friends.0.name'));
 
   return (
     <Form state={state}>
@@ -34,7 +176,7 @@ const App = () => {
           state.submit();
         }}
       >
-        <Field name="name" initialValue="" validate={string}>
+        {/* <Field name="name" initialValue="" onValidate={string}>
           {(name) => (
             <div>
               <div>
@@ -57,31 +199,110 @@ const App = () => {
           )}
         </Field>
 
-        <Field name="email" initialValue="" validate={email}>
+        <Field name="email" initialValue="" onValidate={email}>
           {(email) => (
-            console.log('re-rendered', email.value),
-            (
+            // console.log('re-rendered', email.value),
+            <div>
               <div>
-                <div>
-                  <label htmlFor="email">Email</label>
+                <label htmlFor="email">Email</label>
 
-                  <input
-                    required
-                    type="email"
-                    name="email"
-                    value={email.value}
-                    onBlur={() => email.validate()}
-                    onChange={(e) => email.set(e.target.value)}
-                  />
+                <input
+                  required
+                  type="email"
+                  name="email"
+                  value={email.value}
+                  onBlur={() => email.validate()}
+                  onChange={(e) => email.set(e.target.value)}
+                />
 
-                  <p>{email.state}</p>
-                </div>
-
-                {email.error && <p>{JSON.stringify(email.error)}</p>}
+                <p>{email.state}</p>
               </div>
-            )
+
+              {email.error && <p>{JSON.stringify(email.error)}</p>}
+            </div>
           )}
-        </Field>
+        </Field> */}
+
+        <div>
+          <ul>
+            {Object.keys(friends).map((id, i) => {
+              const value = friends[id];
+              let name = `friends.${id}.name`;
+
+              // console.log(get(state.errors, name), { ...state.errors }, name);
+
+              return (
+                <li key={id}>
+                  <label htmlFor={name}>Friend name</label>
+
+                  <Field name={name} onValidate={email}>
+                    {(f) => {
+                      console.log(f.state, name, f.error);
+
+                      return (
+                        <div>
+                          <input
+                            type="text"
+                            name={name}
+                            value={value.name}
+                            onChange={(e) => {
+                              // state.set('friends', {
+                              //   ...friends,
+                              //   [id]: e.target.value,
+                              // });
+
+                              f.set(e.target.value);
+                            }}
+                          />
+
+                          <button type="button" onClick={() => f.validate()}>
+                            validate
+                          </button>
+
+                          <button
+                            type="button"
+                            onClick={() => {
+                              // friends.splice(i, 1);
+
+                              delete friends[id];
+
+                              state.kill(name);
+
+                              state.set('friends', friends);
+
+                              // const newFriends = friends.filter((f) => f.id !== fr.id);
+
+                              // console.log('newFriends', newFriends);
+
+                              // console.log(`friends.${i}`);
+
+                              // state.kill(`friends.${i}`);
+                            }}
+                          >
+                            remove
+                          </button>
+
+                          {!!f.error && <p>{JSON.stringify(f.error)}</p>}
+                        </div>
+                      );
+                    }}
+                  </Field>
+                </li>
+              );
+            })}
+          </ul>
+
+          <button
+            type="button"
+            onClick={() => {
+              state.set('friends', { ...friends, [nanoid()]: { name: '' } });
+
+              // state.spawn(`friends.${friends.length}`, { name: '' }, email);
+            }}
+          >
+            Add
+          </button>
+        </div>
 
         <button disabled={state.submitted}>
           {state.isSubmitting ? 'submitting' : 'submit'}
@@ -91,4 +312,4 @@ const App = () => {
   );
 };
 
-export default App;
+export default _App;
